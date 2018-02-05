50d63ddab5512238c3913fd49eaa974e56e30ece
Androu <15367779+AndreasRapatas@users.noreply.github.com>
2018-02-05 12:03:23 +0200

    Clean up board.h

--- board.h
+++ board.h
@@ -3,49 +3,62 @@
 
 #include <QWidget>
 #include <QImage>
-#include <QLabel>
+#include <QGraphicsScene>
 #include <QColor>
 #include <random>
 #include <utility>
 
-class Board : public QLabel {
+class Board : public QGraphicsScene {
+
 	Q_OBJECT
+
 private:
+
 	struct Cell {
+
 		enum State : unsigned {
 			PREDITOR = qRgb(255, 0, 0),
-			PREY = qRgb(0, 255, 0),
-			NOTHING = qRgb(0, 0, 0),
+			PREY     = qRgb(0, 255, 0),
+			NOTHING  = qRgb(0, 0, 0),
 		};
+
 		State state;
 		unsigned health;
+
 		Cell() : state(State::NOTHING) {}
 		Cell(State state)
 			: state(state)
-		{
-			health = (state == State::PREDITOR) ? 10 : 0;
-		}
+			, health((state == State::PREDITOR) ? 10 : 0)
+		{}
 	};
+
+	Cell **matrix;
+	QImage *img;
 
 	size_t size_x;
 	size_t size_y;
+	bool running;
+
 	std::default_random_engine generator;
 	std::uniform_int_distribution<int> x_range;
 	std::uniform_int_distribution<int> y_range;
-	Cell **matrix;
-	QImage *img;
-	bool running;
+
 	Board::Cell random_cell();
-	void update();
+	std::pair<unsigned, unsigned> valid_move(unsigned x, unsigned y);
+
 	void fill_random();
 	void fill_other();
 	void clear();
-	std::pair<unsigned, unsigned> valid_move(unsigned x, unsigned y);
+	void update();
+
 public slots:
+
 	void run();
 	void pause();
 	void step();
+
 public:
+
 	Board(size_t x, size_t y, QWidget *parent = 0);
 	~Board();
 };
